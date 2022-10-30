f9e3c23b2b810c07b1c8391951b3d5caf65814ac
Rodrigo <rodrigojmayer@gmail.com>
2022-10-30 16:48:50 +0100

    adding react module project if

--- react.jsx
+++ react.jsx
@@ -51,37 +51,35 @@ const RenderList = props => {
 // }
 function Modal(){
 
-    // const slides = new Array(15)
-    const num_array =  Array.from(Array(15).keys())
-    const project = "auction"
-    const projects = "auctions"
-    const folder = "Auctions"
-
-
-    const images = [
-        {src: '01-Active_listings_without_log_in',         h4: 'Active listings without log in',     h5: 'An unlogged user can view all the auctioned items, but cannot interact with them.'},
-        {src: '02-Article_without_log_in',                 h4: 'Access to articles without log in',  h5: 'You do not have permissions to add an item to your Watch List (favorites). You also cannot bid or ask a question about the item.'},
-        {src: '03-Register',                               h4: 'Register',                           h5: 'In this section we can register our user, in order to have access to the aforementioned options.'},
-        {src: '04-Article_bike_logged_in_bigger_image',    h4: 'Access to article',                  h5: 'When entering the auction of an item, we can see in more detail the image of it and its data.'},
-        {src: '05-Make_question',                          h4: 'Make questions',                     h5: 'We have the option to ask a question, which can only be answered by the creator of the auction of that item.'},
-        {src: '06-Place_bid',                              h4: 'Place bid',                          h5: 'If we are not the creator of the auction and the auction is still open, we may place a bid (higher than the current bid).'},
-        {src: '07-Add_to_watch_list',                      h4: 'Add to watch list',                  h5: 'In this way we save an auction in our Watch List (favorites).'},
-        {src: '08-Categories',                             h4: 'Categories',                         h5: 'The auctions are discriminated by categories, by which we can filter them.'},
-        {src: '09-Post_item',                              h4: 'Post item',                          h5: 'With this form we can load the data of the article that we want to auction (all fields must be completed).'},
-        {src: '10-Log_in_user_owner_of_auction',           h4: 'Log in user owner of auction',       h5: 'We log in with another user to be able to show the options that the creator of an auction has.'},
-        {src: '11-Answer_question',                        h4: 'Answer question',                    h5: 'Here we can see the option to answer a question that we have been asked about our auctioned item.'},
-        {src: '12-Close_auction',                          h4: 'Close auction',                      h5: 'The creator of the auction has the right to close the auction at any time.'},
-        {src: '13-Log_in_auction_winner',                  h4: 'Log in auction winner',              h5: 'We change users again in order to expose what we will see when our offer is the winner of an auction.'},
-        {src: '14-Ipad_responsive',                        h4: 'Tablet responsive',                  h5: 'The entire page adapts perfectly to the size of tablet screens.'},
-        {src: '15-Mobile_responsive',                      h4: 'Mobile responsive',                  h5: 'The entire page adjusts perfectly to the size of mobile screens.'}
-    ]
-
-    const description = 'Design of an eBay-like e-commerce auction site that allows users to post auction listings, bid on listings, comment on those listings, and add listings to a "watch list". Technologies used for this project Python/Django, JS/React, CSS/SASS.'
-    const link = 'https://rjmauctions.pythonanywhere.com/'
-
-
-
-
+    let num_array = new Array(), project = "", projects = "", folder = "", images = new Array, client = "", description = "", link = ""
+    // console.log(prev_project.id)
+    if(prev_project.id == "prev_project_2"){
+        // const slides = new Array(15)
+         num_array =  Array.from(Array(15).keys())
+         project = "auction"
+         projects = "auctions"
+         folder = "Auctions"
+         images = [
+            {src: '01-Active_listings_without_log_in',         h4: 'Active listings without log in',     h5: 'An unlogged user can view all the auctioned items, but cannot interact with them.'},
+            {src: '02-Article_without_log_in',                 h4: 'Access to articles without log in',  h5: 'You do not have permissions to add an item to your Watch List (favorites). You also cannot bid or ask a question about the item.'},
+            {src: '03-Register',                               h4: 'Register',                           h5: 'In this section we can register our user, in order to have access to the aforementioned options.'},
+            {src: '04-Article_bike_logged_in_bigger_image',    h4: 'Access to article',                  h5: 'When entering the auction of an item, we can see in more detail the image of it and its data.'},
+            {src: '05-Make_question',                          h4: 'Make questions',                     h5: 'We have the option to ask a question, which can only be answered by the creator of the auction of that item.'},
+            {src: '06-Place_bid',                              h4: 'Place bid',                          h5: 'If we are not the creator of the auction and the auction is still open, we may place a bid (higher than the current bid).'},
+            {src: '07-Add_to_watch_list',                      h4: 'Add to watch list',                  h5: 'In this way we save an auction in our Watch List (favorites).'},
+            {src: '08-Categories',                             h4: 'Categories',                         h5: 'The auctions are discriminated by categories, by which we can filter them.'},
+            {src: '09-Post_item',                              h4: 'Post item',                          h5: 'With this form we can load the data of the article that we want to auction (all fields must be completed).'},
+            {src: '10-Log_in_user_owner_of_auction',           h4: 'Log in user owner of auction',       h5: 'We log in with another user to be able to show the options that the creator of an auction has.'},
+            {src: '11-Answer_question',                        h4: 'Answer question',                    h5: 'Here we can see the option to answer a question that we have been asked about our auctioned item.'},
+            {src: '12-Close_auction',                          h4: 'Close auction',                      h5: 'The creator of the auction has the right to close the auction at any time.'},
+            {src: '13-Log_in_auction_winner',                  h4: 'Log in auction winner',              h5: 'We change users again in order to expose what we will see when our offer is the winner of an auction.'},
+            {src: '14-Ipad_responsive',                        h4: 'Tablet responsive',                  h5: 'The entire page adapts perfectly to the size of tablet screens.'},
+            {src: '15-Mobile_responsive',                      h4: 'Mobile responsive',                  h5: 'The entire page adjusts perfectly to the size of mobile screens.'}
+        ]
+         client = 'CS50 Harvard'
+         description = 'Design of an eBay-like e-commerce auction site that allows users to post auction listings, bid on listings, comment on those listings, and add listings to a "watch list". Technologies used for this project Python/Django, JS/React, CSS/SASS.'
+         link = 'https://rjmauctions.pythonanywhere.com/'
+    }
 
 
 
@@ -134,7 +132,7 @@ function Modal(){
                     <ul class={'slides-' + projects}>
                         {images.map((image, index) => (
                             <li class={index == 0 ? 'slide slide-' + project : 'slide slide-' + project + ' hide' } id={'image_' + projects + '_' + index}>
-                                <img class="avatar" src={'assets/' + folder + '/' + image.src + '.gif'} alt={'Project ' + projects + ' ' + index+1}/>
+                                <img class="avatar" src={'assets/' + folder + '/' + image.src + '.gif'} alt={'Project ' + folder + ' ' + index+1}/>
                                 <h4 class="text-first-color">{image.h4}</h4>
                                 <h5>{image.h5}</h5>
                             </li>
@@ -150,11 +148,11 @@ function Modal(){
                 </article>
                 <div class="portfolio-info">
 
-                    <h3>Project {projects}</h3>
+                    <h3>Project {folder}</h3>
                     <p> {description} </p>
                     <aside class="portfolio-details">
                         <small class="b-small">Client: </small>
-                        <small>CS50 Harvard</small>
+                        <small>{client}</small>
                         <small class="b-small">Link: </small>
                         <small><a href={link} target="_blank"
                             rel="noopener">{link}</a></small>
